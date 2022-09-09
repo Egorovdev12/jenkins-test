bbd54b3b375283c5bd5c4e4d5a47794012b6f29b
ILYA EGOROV <egorovcf@gmail.com>
2022-09-09 17:51:25 +0300

    com 3

--- src/main/java/com/example/jenkproj/Controller.java
+++ src/main/java/com/example/jenkproj/Controller.java
@@ -8,6 +8,7 @@ import java.util.List;
 @RestController
 public class Controller {
 
+    // базовый слой
     @GetMapping("/hello")
     public List<String> hello() {
         return List.of("Hello", ", World!");
